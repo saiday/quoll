ddb1dc3257a912e3fdca64e0f46dbae4c5c19871
Saiday <feocms@gmail.com>
2018-04-21 17:53:47 +0800

    scraping details

--- quoll/spiders/indievox.py
+++ quoll/spiders/indievox.py
@@ -8,9 +8,29 @@ class IndievoxSpider(scrapy.Spider):
     start_urls = ['https://www.indievox.com/event/ticket/']
 
     def parse(self, response):
-        for event in response.css('div.event-block'):
-            yield {
-                'title': event.css('div.event-data').xpath('h5/a/@title').extract_first(),
-                'info': event.css('div.event-data').xpath('h6/text()').re_first('[\s]{17}(.*)[\s]{12}'),
-                'image': event.xpath('a/img/@src').extract_first()
-            }
+        # for event in response.css('div.event-block'):
+        event = response.css('div.event-block')
+        detail = event.css('div.event-data').xpath('a/@href').extract_first()
+        title = event.css('div.event-data').xpath('h5/a/@title').extract_first()
+        image = event.xpath('a/img/@src').extract_first()
+        meta = {'saved': {'title': title, 'image': image}}
+        yield response.follow(detail, self.parse_detail, meta=meta)
+
+    def parse_detail(self, response):
+        item = response.meta['saved']
+
+        item['body'] = response.css('h1+ div').extract_first()  # html body
+
+        # \n                                            2018/04/21(Sat) 19:30
+        item['date'] = response.css('tr:nth-child(2) td::text')[0].re_first('[\s]{44}(\d{4}\/\d{2}\/\d{2})')
+        item['time'] = response.css('tr:nth-child(2) td::text')[0].re_first('(\d{2}\:\d{2})')
+        item['venue'] = response.css('tr:nth-child(5) td a::text').re_first('\s{53}(.*)\s{48}')
+        item['address'] = response.css('tr:nth-child(6) td::text').re_first('\s{97}(.*)')
+        item['price'] = response.css('tr:nth-child(1) td::text').re_first('\s{45}(.*)\s{40}')
+
+        # might have <a> and raw text element inside artist <td>
+        linked_artists = response.css('tr:nth-child(3) td a::text').extract()
+        unlinked_artists = response.css('tr:nth-child(3) td::text').re('\w{2,}')
+        item['artists'] = linked_artists + unlinked_artists
+
+        yield item
