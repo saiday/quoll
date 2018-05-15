954c94f894449c0f2a8d4ae4866dd23eb170d0e9
Saiday <feocms@gmail.com>
2018-05-16 02:29:37 +0800

    db schema

--- quoll/pipelines.py
+++ quoll/pipelines.py
@@ -4,6 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import logging
 
 import pymysql
 
@@ -23,7 +24,33 @@ class QuollPipeline(object):
         self.cursor = self.connect.cursor()
 
     def process_item(self, item, spider):
-
+        try:
+            query = """select * from event where url = %s"""
+            self.cursor.execute(query, item['url'])
+            exist = self.cursor.fetchone()
+            if exist:
+                self.cursor.execute("""update event set title = %s, url = %s, image = %s, body = %s, date = %s, time = %s, price = %s""",
+                                    (item['title'],
+                                     item['url'],
+                                     item['image'],
+                                     item['body'],
+                                     item['date'],
+                                     item['time'],
+                                     item['price']))
+            else:
+                self.cursor.execute("""insert into event(title, url, image, body, date, time, price) value (%s, %s, %s, %s, %s, %s, %s)""",
+                                    (item['title'],
+                                     item['url'],
+                                     item['image'],
+                                     item['body'],
+                                     item['date'],
+                                     item['time'],
+                                     item['price']))
+            self.connect.commit()
+        except Exception as error:
+            # TODO: slack hook
+            print(error)
+            logging.critical(error)
 
         return item
 
